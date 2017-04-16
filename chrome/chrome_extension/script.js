1cafbc242b7b96f1caae978fe08af10dfb01cfde
Drake Witt <dwitt@dranweb.com>
2017-04-16 10:55:51 -0400

    Update script.js

--- chrome/chrome_extension/script.js
+++ chrome/chrome_extension/script.js
@@ -26,7 +26,7 @@ var title = (document.title);
 
         http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
+            alert(http.responseText.content);
             }
         }
         http.send();
